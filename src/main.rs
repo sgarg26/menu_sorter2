cff5dfb1ae1a8f46b3dfaa795b1ce1498e0675ef
Siddharth <siddharth261202@gmail.com>
2024-12-30 10:35:38 -0500

    fixed cwd check path

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use std::{env, process};
 
 fn check_cwd() -> bool {
     let cwd = env::current_dir().unwrap().to_path_buf();
-    if !cwd.ends_with("The Witcher 3") {
-        println!("Please run this script from the ../../The Witcher 3 directory");
+    if !cwd.ends_with("user_config_matrix/pc") {
+        println!("Please run this script from the ../../user_config_matrix/pc directory");
         return false;
     }
     true
 }
 
 fn main() {
-    // first check if we're currently in the ../../The Witcher 3 directory
+    // first check if we're currently in the ../../The Witcher 3/../pc dir
     if !check_cwd() {
         process::exit(1);
     }
