0ff2e9237d08351d5c496256293e3ae1b9ca4dd4
Siddharth <siddharth261202@gmail.com>
2024-12-30 02:18:20 -0500

    added check for cwd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::stdin;
+use std::{env, process};
+
+fn check_cwd() -> bool {
+    let cwd = env::current_dir().unwrap().to_path_buf();
+    if !cwd.ends_with("The Witcher 3") {
+        println!("Please run this script from the ../../The Witcher 3 directory");
+        return false;
+    }
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    // first check if we're currently in the ../../The Witcher 3 directory
+    if !check_cwd() {
+        process::exit(1);
+    }
 }
