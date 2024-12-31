32e6e6ec799dbbf4d40e4a2732265fb0824ef316
Siddharth <siddharth261202@gmail.com>
2024-12-31 10:16:17 -0500

    User can pick category instead of typing it out, safer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{stdin, Write, BufRead, BufReader, BufWriter};
 use std::{env, fs, path, process};
 
+use colored::Colorize;
+
 const DEBUG: bool = true;
 
 fn check_cwd() -> bool {
@@ -18,6 +20,23 @@ fn get_user_file() -> path::PathBuf {
     buffer = buffer.trim().to_string();
     let path = path::Path::new(&buffer);
     path.to_path_buf()
+}
+
+fn get_category() -> String {
+    let categories = ["alchemy_and_equipment", "camera", "characters", "combat", "gameplay", "quests_and_adventures", "user_interface", "visuals_and_graphics", "miscellaneous"];
+    let mut count = 1;
+    for category in categories {
+        println!("{}. {}", count, category);
+        count += 1;
+    }
+    
+    println!("{}", "Please select a category by entering the corresponding number:".green());
+    let mut c = String::new();
+    stdin().read_line(&mut c).unwrap();
+    c = c.trim().to_string();
+    let category: usize = c.parse::<usize>().unwrap();
+    println!("category chosen is {}: {}", category, categories[category-1]);
+    return categories[category-1].to_string();
 }
 
 fn main() {
@@ -40,6 +59,9 @@ fn main() {
     
     println!("File opened successfully");
     
+    // Get the category the user wants to add to.
+    let category = get_category();
+    
     // Read the file into a buffer to edit.
     // Using BufReader, more efficient when reading line by line.
     // Better in this case, bc end goal is to see if a certain line contains 'Mods...'
@@ -48,6 +70,7 @@ fn main() {
     // When adding to a category, category is always added right after 'Mods.'
     // The sequence is unique, so we can use it to find the line we want to edit.
     let sequence = "Mods.";
+    let rep = format!("{}{}", sequence, category);
     
     // We'll write to a temp file, then overwrite the original file with the temp file.
     let tmp_path = format!("{}.tmp", path.display());
@@ -58,7 +81,7 @@ fn main() {
     
     for line in file.lines() {
         let line = line.expect("Unable to read line");
-        let mut new_line = line.replace(sequence, "Mods.camera.");
+        let mut new_line = line.replace(sequence, &rep);
         new_line.push('\n'); // new line char gets removed for some reason? not sure why. 
         tmp_file.write_all(new_line.as_bytes()).expect("Unable to write data")
     }
